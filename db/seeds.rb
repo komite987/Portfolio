8b52d9cae74b3879d5f3ea8a2205ce9cc5d16ba1
Komite <komait.salamah@gmail.com>
2020-04-13 00:05:37 +0300

    add index action to portfolio

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,32 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+10.times do |post|
+	Post.create!(
+		title: "my post #{post}",
+		body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Dolor sit amet consectetur adipiscing elit. Auctor elit sed vulputate mi sit amet mauris. Sit amet justo donec enim diam. Lobortis feugiat vivamus at augue eget arcu."
+		)
+end
+
+puts "10 posts created"
+
+5.times do |skill|
+	Skill.create!(
+		title: "Rails #{skill}",
+		percent: 15
+		)
+end
+
+puts "5 skills created"
+
+
+9.times do |portfolio|
+	Portfolio.create!(
+		title: "My portfolio #{portfolio}",
+		subtitle: "great service",
+		body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+		main_img: "https://place-hold.it/600x300",
+		thumb_img: "https://place-hold.it/300x200"
+
+		)
+end
+
+puts "9 portfolios created"
+
