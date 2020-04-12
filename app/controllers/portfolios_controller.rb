716ad81a31f922ca0f8596cea5820bb2b8598e8d
Komite <komait.salamah@gmail.com>
2020-04-13 01:49:42 +0300

    add portfolio form and create action

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -4,23 +4,30 @@ class PortfoliosController < ApplicationController
 		@portfolio_items = Portfolio.all
 	end
 
+	def new
+		@portfolio_item = Portfolio.new
+	end
 
 
+	def create
+		@portfolio_item = Portfolio.new(portfolio_params)
+	    respond_to do |format|
+	      if @portfolio_item.save
+	        format.html { redirect_to portfolios_path, notice: 'Portfolio item was successfully created.' }
+	      else
+	        format.html { render :new }
+	      end
+	    end
+	end
+
+	  def show
+	  	@portfolio_item = Portfolio.find(params[:id])
+	  end
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+private 
+	def portfolio_params
+		params.require(:portfolio).permit(:title, :subtitle, :body)
+	end
 
 end
