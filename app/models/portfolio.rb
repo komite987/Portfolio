f154db78ef2e38c41c857dd16b896771c52b99eb
Komite <komait.salamah@gmail.com>
2020-04-17 22:09:58 +0300

    add scope to portfolio

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -1,3 +1,10 @@
 class Portfolio < ApplicationRecord
     validates_presence_of :title, :body, :main_img, :thumb_img
+
+
+    def self.angular
+    	Portfolio.where(subtitle: "Angular")    	
+    end
+
+    scope :Ruby_on_rails, -> { where(subtitle: "Ruby on Rails")}
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,16 @@
+3.times do |topic|
+	Topic.create!(
+		title: "Topic #{topic}"
+		)
+end
+
+puts "3 topics created"
+
 10.times do |post|
 	Post.create!(
 		title: "my post #{post}",
-		body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Dolor sit amet consectetur adipiscing elit. Auctor elit sed vulputate mi sit amet mauris. Sit amet justo donec enim diam. Lobortis feugiat vivamus at augue eget arcu."
+		body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Dolor sit amet consectetur adipiscing elit. Auctor elit sed vulputate mi sit amet mauris. Sit amet justo donec enim diam. Lobortis feugiat vivamus at augue eget arcu.",
+		topic_id: Topic.last.id
 		)
 end
 
@@ -17,14 +26,23 @@ end
 puts "5 skills created"
 
 
-9.times do |portfolio|
+7.times do |portfolio|
 	Portfolio.create!(
 		title: "My portfolio #{portfolio}",
-		subtitle: "great service",
+		subtitle: "Ruby on Rails",
 		body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
 		main_img: "https://place-hold.it/600x300",
 		thumb_img: "https://place-hold.it/300x200"
+		)
+end
 
+	2.times do |portfolio|
+	Portfolio.create!(
+		title: "My portfolio #{portfolio}",
+		subtitle: "Angular",
+		body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+		main_img: "https://place-hold.it/600x300",
+		thumb_img: "https://place-hold.it/300x200"
 		)
 end
 
