22216e84e6356324ac1f8f91b645d17cf543eeb9
Komite <komait.salamah@gmail.com>
2020-04-18 00:43:57 +0300

    add tecnology model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_17_202444) do
+ActiveRecord::Schema.define(version: 2020_04_17_212457) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -77,6 +77,14 @@ ActiveRecord::Schema.define(version: 2020_04_17_202444) do
     t.text "badge"
   end
 
+  create_table "technologies", force: :cascade do |t|
+    t.string "name"
+    t.bigint "portfolio_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["portfolio_id"], name: "index_technologies_on_portfolio_id"
+  end
+
   create_table "topics", force: :cascade do |t|
     t.string "title"
     t.datetime "created_at", precision: 6, null: false
@@ -84,4 +92,5 @@ ActiveRecord::Schema.define(version: 2020_04_17_202444) do
   end
 
   add_foreign_key "posts", "topics"
+  add_foreign_key "technologies", "portfolios"
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,50 +1,59 @@
-3.times do |topic|
-	Topic.create!(
-		title: "Topic #{topic}"
+# 3.times do |topic|
+# 	Topic.create!(
+# 		title: "Topic #{topic}"
+# 		)
+# end
+
+# puts "3 topics created"
+
+# 10.times do |post|
+# 	Post.create!(
+# 		title: "my post #{post}",
+# 		body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Dolor sit amet consectetur adipiscing elit. Auctor elit sed vulputate mi sit amet mauris. Sit amet justo donec enim diam. Lobortis feugiat vivamus at augue eget arcu.",
+# 		topic_id: Topic.last.id
+# 		)
+# end
+
+# puts "10 posts created"
+
+# 5.times do |skill|
+# 	Skill.create!(
+# 		title: "Rails #{skill}",
+# 		percent: 15
+# 		)
+# end
+
+# puts "5 skills created"
+
+
+# 7.times do |portfolio|
+# 	Portfolio.create!(
+# 		title: "My portfolio #{portfolio}",
+# 		subtitle: "Ruby on Rails",
+# 		body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+# 		main_img: "https://place-hold.it/600x300",
+# 		thumb_img: "https://place-hold.it/300x200"
+# 		)
+# end
+
+# 	2.times do |portfolio|
+# 	Portfolio.create!(
+# 		title: "My portfolio #{portfolio}",
+# 		subtitle: "Angular",
+# 		body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+# 		main_img: "https://place-hold.it/600x300",
+# 		thumb_img: "https://place-hold.it/300x200"
+# 		)
+# end
+
+# puts "9 portfolios created"
+
+
+3.times do |technology|
+	Technology.create!(
+		name: "Technology #{technology}",
+		portfolio_id: Portfolio.last.id
 		)
 end
 
-puts "3 topics created"
-
-10.times do |post|
-	Post.create!(
-		title: "my post #{post}",
-		body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Dolor sit amet consectetur adipiscing elit. Auctor elit sed vulputate mi sit amet mauris. Sit amet justo donec enim diam. Lobortis feugiat vivamus at augue eget arcu.",
-		topic_id: Topic.last.id
-		)
-end
-
-puts "10 posts created"
-
-5.times do |skill|
-	Skill.create!(
-		title: "Rails #{skill}",
-		percent: 15
-		)
-end
-
-puts "5 skills created"
-
-
-7.times do |portfolio|
-	Portfolio.create!(
-		title: "My portfolio #{portfolio}",
-		subtitle: "Ruby on Rails",
-		body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-		main_img: "https://place-hold.it/600x300",
-		thumb_img: "https://place-hold.it/300x200"
-		)
-end
-
-	2.times do |portfolio|
-	Portfolio.create!(
-		title: "My portfolio #{portfolio}",
-		subtitle: "Angular",
-		body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-		main_img: "https://place-hold.it/600x300",
-		thumb_img: "https://place-hold.it/300x200"
-		)
-end
-
-puts "9 portfolios created"
-
+puts "3 technologies created"
