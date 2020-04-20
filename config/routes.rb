06613a1f00a116141d4784a9d32a8ede5e70ba4f
Komite <komait.salamah@gmail.com>
2020-04-20 19:09:13 +0300

    complete devise installition and custom routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
 
+  devise_for :users, path: '', path_names: { sign_in: 'login', sign_out: 'logout', sign_up: 'register'}
   resources :portfolios, except: [:show]
 	get 'portfolio/:id', to: 'portfolios#show', as: 'portfolio_show'
 
@@ -14,11 +15,7 @@ Rails.application.routes.draw do
       get :toggle_status
     end
   end
-
-
-
-
-
+  
   resources :paragraphs
   resources :articles
   resources :comments
