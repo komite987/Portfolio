4726e73f32d798158f2860f548052850e8c4e9ff
Komite <komait.salamah@gmail.com>
2020-04-22 20:54:58 +0300

    finish controller branch with null object pattern

--- config/application.rb
+++ config/application.rb
@@ -23,13 +23,7 @@ module Myapp
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
-
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration can go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded after loading
-    # the framework and any gems in your application.
-
-    # Don't generate system test files.
+    # config.action_controller.permit_all_parameters = true
     config.generators.system_tests = nil
   end
 end
