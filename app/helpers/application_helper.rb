616f31dc12d4721f98d8737367367552da69a953
Komite <komait.salamah@gmail.com>
2020-04-29 02:25:03 +0300

    styling forms

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,6 @@
 module ApplicationHelper
 
-  def nav_links style
+  def nav_links style = ""
     if current_user.is_a?(GuestUser)
       (link_to 'Log in', new_user_session_path, class: style) + " ".html_safe +
       (link_to 'Sign up', new_user_registration_path, class: style)
