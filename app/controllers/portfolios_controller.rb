a85c7510a17b41ad19261c012432b1f1bc8b5301
Komite <komait.salamah@gmail.com>
2020-05-01 22:32:08 +0300

    end styling

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -10,7 +10,7 @@ class PortfoliosController < ApplicationController
 
 	def new
 		@portfolio_item = Portfolio.new
-		4.times { @portfolio_item.technologies.build }
+		3.times { @portfolio_item.technologies.build }
 	end
 
 	def create
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,7 +2,7 @@ module ApplicationHelper
 
   def nav_links style = ""
     if current_user.is_a?(GuestUser)
-      (link_to 'Log in', new_user_session_path, class: style) + " ".html_safe +
+      (link_to 'Log in', new_user_session_path, class: style) + "  ".html_safe +
       (link_to 'Sign up', new_user_registration_path, class: style)
     else 
       link_to 'Log out', destroy_user_session_path, method: :delete, class: style
@@ -18,5 +18,28 @@ module ApplicationHelper
 
   def copyright_generator
     KomaitViewTool::Renderer.copyright "Komait", "All rights reserved"
+  end
+
+  def nav_items 
+ [ {url: root_path, title: 'Home'},
+      {url: about_path, title: 'About Me'},
+      {url: contact_path, title: 'Contact us'},
+      {url: posts_path, title: 'Blog'},
+      {url: portfolios_path, title: 'Portfolio'}
+    ]
+  end
+  
+  def nav_helper style, tag
+    nav_links = '' 
+
+    nav_items.each do |item|
+      nav_links << "<#{tag}><a href='#{item[:url]}' 
+      class='#{style} #{active? item[:url]}'>#{item[:title]}</a></#{tag}>"
+    end
+  nav_links.html_safe
+  end
+
+  def active? path
+    "active" if current_page? path    
   end
 end
