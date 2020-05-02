15b9a1dbdf75f7238b6d24beace373fc471a3853
Komite <komait.salamah@gmail.com>
2020-05-02 13:29:48 +0300

    add position to portfolio and html ssortable

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_26_164802) do
+ActiveRecord::Schema.define(version: 2020_05_01_211541) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -55,6 +55,7 @@ ActiveRecord::Schema.define(version: 2020_04_26_164802) do
     t.text "thumb_img"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "position"
   end
 
   create_table "posts", force: :cascade do |t|
