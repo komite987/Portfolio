a459676770193d81527184b10760a46241125c46
Komite <komait.salamah@gmail.com>
2020-05-10 03:29:00 +0300

    delete comment scaffold

--- config/routes.rb
+++ config/routes.rb
@@ -24,6 +24,5 @@ Rails.application.routes.draw do
   
   resources :paragraphs
   resources :articles
-  resources :comments
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,19 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_03_112142) do
+ActiveRecord::Schema.define(version: 2020_05_09_183030) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "articles", force: :cascade do |t|
-    t.string "title"
-    t.text "body"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
-
-  create_table "comments", force: :cascade do |t|
     t.string "title"
     t.text "body"
     t.datetime "created_at", precision: 6, null: false
